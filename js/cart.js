e5130fb7f61eaf5b1af8d10fd427624956494191
maxsmrnv <smirnov.m4ksim@gmail.com>
2018-02-06 01:26:01 +0300

    cart update

--- js/cart.js
+++ js/cart.js
@@ -17,32 +17,27 @@ class CartItem {
 }
 
 
-function* counter () {
-    for (let i =1;; i++){
-        yield i
-    }
 
-}
 
 let cart = {
     cartItems: {},
 
-    drawItem: function (item) {
+    drawItem: function (item,callback) {
 
         let itemContainer = document.createElement('div');
         itemContainer.classList.add("d-flex", "item-container_", "justify-content-center", "my-1");
 
         let itemTemplate =
-            `<img class="photo_" src="${this.itemPhoto}"/>
+            `<img class="photo_" src="${item.itemPhoto}"/>
             <div class="d-flex flex-column justify-content-center">
-                <div>${this.itemName}</div>
+                <div class="cart-item-name_">${item.itemName}</div>
                 <div>
                     <span>PRICE</span>
-                    <span>${this.itemPrice}</span>
+                    <span>${item.itemPrice}</span>
                 </div>
                 <div>
                     <span>QTY:</span>
-                    <input type="number" class="item-qty_" value="${this.qty}">
+                    <input type="number" class="item-qty_" value="${item.qty}">
                 </div>
             </div>
             <!--<i class="remove-item_ fas fa-times"></i>-- Не смог разобраться, как повесить обработчик на этот элемент, поэтому сделал просто кнопку>-->
@@ -51,12 +46,14 @@ let cart = {
         itemContainer.innerHTML = itemTemplate.trim();
 
         cartMenu.appendChild(itemContainer);
+        callback();
+
+
     },
 
     addToCart: function (e) {
         let element = e.target.parentNode;
         let item = new CartItem(element);
-        let itemCounter = counter();
 
         let promise = new Promise(function (resolve, reject) {
 
@@ -71,26 +68,32 @@ let cart = {
         console.log(promise);
         promise.then(
             (function () {
-                cart.cartItems[item.itemName]=itemCounter.next();
+                cart.drawItem(item,(function () {
+                    cart.cartItems[item.itemName]=1;
+                }));
                 console.log('add');
+                setHandler(document.querySelectorAll('.remove-item_'), "click", cart.removeFromCart)
             }),
-            (function () {
-
-                console.log(`cartItems: ${cart.cartItems}`);
-                console.log(cart.cartItems[item.itemName]);
-            })
+            cart.updateQty
         );
         // console.log(cart.cartItems)
 
 
-        // setHandler(document.querySelectorAll('.remove-item_'), "click", cart.removeFromCart);
+        // ;
     },
 
     removeFromCart: function (e) {
+        let element = e.target.parentNode;
+        console.log(element);
+        let itemName = element.querySelectorAll('.cart-item-name_')[0].innerHTML.trim();
+        element.parentNode.removeChild(element);
+        delete cart.cartItems[itemName]
 
     },
 
-    getItemCount: function () {
+    updateQty: function () {
+        console.log('QQ');
+        let curCnt = document.querySelectorAll('.item-qty_')[0].value;
 
     }
 
