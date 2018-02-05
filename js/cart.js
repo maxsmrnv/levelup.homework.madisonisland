ba2af076d2a77f3b1dc559c3f4c7264a71976e4e
Maksim.Smirnov <Maksim.Smirnov@billing.ru>
2018-02-05 19:24:04 +0300

    refactoring

--- js/cart.js
+++ js/cart.js
@@ -5,33 +5,44 @@ const addToCartBtns = document.querySelectorAll('.btn-custom_');
 const cartMenu = document.querySelector('#added-items');
 
 
-
 class CartItem {
 
     constructor(element) {
-        this.itemPhoto = element.querySelectorAll('.photo_')[0].innerHTML;
-        this.itemName = element.querySelectorAll('.item-name_')[0].innerHTML;
-        this.itemPrice = element.querySelectorAll('.price_')[0].innerHTML;
+        this.itemPhoto = element.getElementsByClassName('photo_')[0].getAttribute('src');
+        this.itemName = element.querySelectorAll('.item-name_')[0].innerHTML.trim();
+        this.itemPrice = element.querySelectorAll('.price_')[0].innerHTML.trim();
+        this.qty = 1;
     }
 
+}
 
-    buildCartItem(qty = 1) {
+
+function* counter () {
+    for (let i =1;; i++){
+        yield i
+    }
+
+}
+
+let cart = {
+    cartItems: {},
+
+    drawItem: function (item) {
 
         let itemContainer = document.createElement('div');
         itemContainer.classList.add("d-flex", "item-container_", "justify-content-center", "my-1");
 
-        let itemTemplate = `
-<!--<button type="button" class="remove-item_ btn text-uppercase">del</button>-->
-            ${this.itemPhoto}
+        let itemTemplate =
+            `<img class="photo_" src="${this.itemPhoto}"/>
             <div class="d-flex flex-column justify-content-center">
-                <span>${this.itemName}</span>
+                <div>${this.itemName}</div>
                 <div>
                     <span>PRICE</span>
                     <span>${this.itemPrice}</span>
                 </div>
                 <div>
                     <span>QTY:</span>
-                    <input type="number" class="item-qty_" value="${qty}">
+                    <input type="number" class="item-qty_" value="${this.qty}">
                 </div>
             </div>
             <!--<i class="remove-item_ fas fa-times"></i>-- Не смог разобраться, как повесить обработчик на этот элемент, поэтому сделал просто кнопку>-->
@@ -39,34 +50,44 @@ class CartItem {
 
         itemContainer.innerHTML = itemTemplate.trim();
 
-        return itemContainer
-    }
-}
-
-let cart = {
-    itemsOfCart: {},
+        cartMenu.appendChild(itemContainer);
+    },
 
     addToCart: function (e) {
-        let element = e.target.parentElement;
+        let element = e.target.parentNode;
         let item = new CartItem(element);
-        if (cart.itemsOfCart[item.itemName.trim()] === undefined) {
-            cart.itemsOfCart[item.itemName.trim()] = 1;
-            cartMenu.appendChild(item.buildCartItem());
-        }
-        else {
-            console.log(item)
-        }
+        let itemCounter = counter();
+
+        let promise = new Promise(function (resolve, reject) {
+
+            if (cart.cartItems[item.itemName]!== undefined) {
+                return reject();
+            }
+            else {
+                return resolve();
+            }
+
+        });
+        console.log(promise);
+        promise.then(
+            (function () {
+                cart.cartItems[item.itemName]=itemCounter.next();
+                console.log('add');
+            }),
+            (function () {
+
+                console.log(`cartItems: ${cart.cartItems}`);
+                console.log(cart.cartItems[item.itemName]);
+            })
+        );
+        // console.log(cart.cartItems)
 
 
-        setHandler(document.querySelectorAll('.remove-item_'), "click", cart.removeFromCart);
+        // setHandler(document.querySelectorAll('.remove-item_'), "click", cart.removeFromCart);
     },
 
     removeFromCart: function (e) {
-        let element = e.target.parentElement;
-        // let itemName = document.querySelectorAll('.item-name_')[0].innerHTML;
-        element.parentNode.removeChild(element);
-        delete cart.itemsOfCart[item.itemName.trim()];
-        console.log(cart.itemsOfCart)
+
     },
 
     getItemCount: function () {
